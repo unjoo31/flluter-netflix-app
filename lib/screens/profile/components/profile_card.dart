b3310f83365e4a5805d8835dd15b9b680c81b6ff
unjoo31 <unjoo31@gmail.com>
2024-02-28 10:30:31 +0900

    프로필화면 위젯 작성하기

---
+++ lib/screens/profile/components/profile_card.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ProfileCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Column(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(5.0),
+            child: Image(
+              image: AssetImage("assets/images/dog.jpg"),
+              width: 100.0,
+              height: 100.0,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            "언주",
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+      ),
+      onTap: () {},
+    );
+  }
+}
