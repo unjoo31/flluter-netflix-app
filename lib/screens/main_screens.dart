760dd370b26c8a2ab1c7d580a81426a33b34b221
unjoo31 <unjoo31@gmail.com>
2024-02-28 14:15:49 +0900

    메인 화면 > 위젯 연결 완료

--- lib/screens/main_screens.dart
+++ lib/screens/main_screens.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:netflix/constants.dart';
+import 'package:netflix/models/nav_item.dart';
+import 'package:netflix/screens/coming/coming_screen.dart';
+import 'package:netflix/screens/contents_list/contents_list_screen.dart';
+import 'package:netflix/screens/home/home_screen.dart';
 
 class MainScreens extends StatefulWidget {
   static String routeName = "/main_screens";
@@ -8,11 +13,35 @@ class MainScreens extends StatefulWidget {
 }
 
 class _MainScreensState extends State<MainScreens> {
+  int _selectedIndex = 0;
+  List pages = [
+    HomeScreen(),
+    ComingScreen(),
+    ContentsListScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("MainScreens"),
+      body: pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.black,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: kDarkColor,
+        selectedFontSize: 12.0,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: List.generate(
+          navItems.length,
+          (index) => BottomNavigationBarItem(
+            icon: Icon(navItems[index].icon),
+            label: navItems[index].label,
+          ),
+        ),
       ),
     );
   }
