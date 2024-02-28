e7a939dc994d83aee694710d83f877b7b913872a
unjoo31 <unjoo31@gmail.com>
2024-02-28 10:21:18 +0900

    프로필화면 기본코드 작성

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:netflix/constants.dart';
 
 class ProfileScreen extends StatelessWidget {
   static String routeName = "/profile";
@@ -6,8 +8,28 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        centerTitle: true,
+        title: Text(
+          "MODU PLAY",
+          style: kTitleTextStyle,
+        ),
+        actions: [
+          Icon(FontAwesomeIcons.pen, size: 18.0),
+          const SizedBox(width: 12.0),
+        ],
+      ),
       body: Center(
-        child: Text("ProfileScreen"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "모두의 플레이를 시청할 프로필을 선택하세요.",
+              style: TextStyle(color: Colors.white, fontSize: 18.0),
+            ),
+          ],
+        ),
       ),
     );
   }
