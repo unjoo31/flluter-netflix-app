1bf1fe2a41f1b903013b3df6b6bd52d3fca26686
unjoo31 <unjoo31@gmail.com>
2024-06-25 21:52:48 +0900

    동영상 플레이어 위젯 추가하기

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,5 +1,7 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:video_player/video_player.dart';
 
 class DetailScreen extends StatefulWidget {
   static String routeName = "/detail";
@@ -9,6 +11,40 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  List<String> posters = [
+    "assets/images/big_buck_bunny_poster.jpg",
+    "assets/images/les_miserables_poster.jpg",
+    "assets/images/minari_poster.jpg",
+    "assets/images/the_book_of_fish_poster.jpg",
+  ];
+
+  late VideoPlayerController _videoPlayerController;
+  ChewieController? _chewieController;
+
+  Future<void> initializePlayer() async {
+    _videoPlayerController = VideoPlayerController.network(
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4");
+    await Future.wait([_videoPlayerController.initialize()]);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: true,
+    );
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializePlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,8 +65,23 @@ class _DetailScreenState extends State<DetailScreen> {
           SizedBox(width: 15.0),
         ],
       ),
-      body: Center(
-        child: Text("DetailScreen"),
+      body: Column(
+        children: [
+          Container(
+            height: 230.0,
+            child: _chewieController != null &&
+                    _chewieController!.videoPlayerController.value.isInitialized
+                ? Chewie(controller: _chewieController!)
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 20),
+                      Text('Loading'),
+                    ],
+                  ),
+          ),
+        ],
       ),
     );
   }
