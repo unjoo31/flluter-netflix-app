5eb9604563c54a1485b54dc12562cf6215883949
unjoo31 <unjoo31@gmail.com>
2024-06-25 21:45:02 +0900

    상세정보 화면 > routes에 추가하기

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,8 +3,11 @@ import 'package:netflix/screens/main_screens.dart';
 import 'package:netflix/screens/profile/profile_screen.dart';
 import 'package:netflix/screens/splash/splash_screen.dart';
 
+import 'screens/detail/detail_screen.dart';
+
 final Map<String, WidgetBuilder> route = {
   SplashScreen.routeName: (context) => SplashScreen(),
   ProfileScreen.routeName: (context) => ProfileScreen(),
   MainScreens.routeName: (context) => MainScreens(),
+  DetailScreen.routeName: (context) => DetailScreen(),
 };
--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DetailScreen extends StatefulWidget {
-  static String routName = "/detail";
+  static String routeName = "/detail";
 
   @override
   State<DetailScreen> createState() => _DetailScreenState();
