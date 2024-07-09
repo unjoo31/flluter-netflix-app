fbf4688edeca4cde10e11bc7b72de5d5bf7c5ece
unjoo31 <unjoo31@gmail.com>
2024-07-09 20:48:11 +0900

    nestedscrollview로 tapbar를 화면 중간에 추가하기

--- lib/screens/detail/detail_screen.dart
+++ lib/screens/detail/detail_screen.dart
@@ -1,6 +1,11 @@
 import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:netflix/constants.dart';
+import 'package:netflix/models/episode.dart';
+import 'package:netflix/screens/components/label_icon.dart';
+import 'package:netflix/screens/components/play_button.dart';
+import 'package:netflix/screens/components/small_sub_text.dart';
 import 'package:video_player/video_player.dart';
 
 class DetailScreen extends StatefulWidget {
@@ -83,6 +88,167 @@ class _DetailScreenState extends State<DetailScreen> {
                     ],
                   ),
           ),
+          Expanded(
+            child: DefaultTabController(
+              length: 2,
+              child: NestedScrollView(
+                headerSliverBuilder: (context, value) {
+                  return [
+                    SliverToBoxAdapter(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              " 빅 벅 버니",
+                              style: TextStyle(fontSize: 24.0),
+                            ),
+                            SizedBox(height: 10.0),
+                            Row(
+                              children: [
+                                Text(
+                                  "95% 일치",
+                                  style: TextStyle(
+                                    fontSize: 12.0,
+                                    color: Colors.green,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                                SmallSubText(text: "2008"),
+                                SizedBox(width: 10.0),
+                                Container(
+                                  decoration: BoxDecoration(color: kLightColor),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 4.0, vertical: 2.0),
+                                    child: Text(
+                                      "15+",
+                                      style: TextStyle(
+                                          fontSize: 12.0,
+                                          color: Colors.white60),
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(width: 10.0),
+                                SmallSubText(text: "시즌 1개"),
+                              ],
+                            ),
+                            SizedBox(height: 6.0),
+                            Text(
+                              "오늘 한국에서 콘텐츠 순위 1위",
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                            SizedBox(height: 10.0),
+                            PlayButton(width: double.infinity),
+                            SizedBox(height: 10.0),
+                            Container(
+                              width: double.infinity,
+                              height: 35.0,
+                              decoration: BoxDecoration(
+                                color: kButtonDarkColor,
+                                borderRadius: BorderRadius.circular(4.0),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    FontAwesomeIcons.download,
+                                    size: 16.0,
+                                  ),
+                                  SizedBox(width: 10.0),
+                                  Text(
+                                    "저장",
+                                  )
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10.0),
+                            Text(episodes[0].description),
+                            SizedBox(height: 6.0),
+                            RichText(
+                              text: TextSpan(
+                                  text: "출연: ",
+                                  style: TextStyle(
+                                      fontSize: 12.0, color: Colors.grey[300]),
+                                  children: [
+                                    TextSpan(
+                                        text: "버니, 프랭크, 링키, 기메라... ",
+                                        style: TextStyle(color: Colors.grey)),
+                                    TextSpan(text: "더보기"),
+                                  ]),
+                            ),
+                            SizedBox(height: 20.0),
+                            Container(
+                              width: MediaQuery.of(context).size.width * 0.6,
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  LabelIcon(
+                                    icon: FontAwesomeIcons.plus,
+                                    label: "내가 찜한 콘텐츠",
+                                  ),
+                                  LabelIcon(
+                                    icon: FontAwesomeIcons.thumbsUp,
+                                    label: "평가",
+                                  ),
+                                  LabelIcon(
+                                    icon: FontAwesomeIcons.shareAlt,
+                                    label: "공유",
+                                  )
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    SliverPadding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      sliver: SliverToBoxAdapter(
+                        child: Container(
+                          height: 50.0,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              top: BorderSide(color: kLightColor),
+                            ),
+                          ),
+                          // 3
+                          child: TabBar(
+                            indicator: UnderlineTabIndicator(
+                              borderSide:
+                                  BorderSide(color: Colors.red, width: 5.0),
+                              insets: EdgeInsets.only(bottom: 45.0),
+                            ),
+                            isScrollable: true,
+                            tabs: [
+                              Tab(
+                                text: "회차 정보",
+                              ),
+                              Tab(
+                                text: "비슷한 콘텐츠",
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    )
+                  ];
+                },
+                body: Container(
+                  // 4
+                  child: TabBarView(
+                    children: [
+                      Container(),
+                      Container(),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          )
         ],
       ),
     );
