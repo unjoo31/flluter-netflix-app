03340a9c01933b29b636a62d6dfed950f6fd11ee
unjoo31 <unjoo31@gmail.com>
2024-07-17 21:00:18 +0900

    공개예정화면 만들기 > 동영상 플레이어 추가하기

--- lib/screens/coming/coming_screen.dart
+++ lib/screens/coming/coming_screen.dart
@@ -1,5 +1,7 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:video_player/video_player.dart';
 
 class ComingScreen extends StatefulWidget {
   @override
@@ -7,6 +9,35 @@ class ComingScreen extends StatefulWidget {
 }
 
 class _ComingScreenState extends State<ComingScreen> {
+  late VideoPlayerController _videoPlayerController;
+  ChewieController? _chewieController;
+
+  final List<String> genres = ["가슴 뭉쿨", "풍부한 감정", "권선징악", "영화"];
+
+  Future<void> initializePlayer() async {
+    _videoPlayerController = VideoPlayerController.network(
+        'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4');
+    await Future.wait([_videoPlayerController.initialize()]);
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: true,
+    );
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initializePlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,6 +61,27 @@ class _ComingScreenState extends State<ComingScreen> {
                 image: AssetImage("assets/images/dog.jpg"),
               ),
             ),
+          ),
+          SizedBox(width: 15.0),
+        ],
+      ),
+      body: ListView(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.3,
+            child: _chewieController != null &&
+                    _chewieController!.videoPlayerController.value.isInitialized
+                ? Chewie(
+                    controller: _chewieController!,
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 20),
+                      Text('Loading'),
+                    ],
+                  ),
           ),
         ],
       ),
